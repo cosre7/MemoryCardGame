49e59a2acbe8eff74c13b91ea9ca41f4108329ce
cosre7 <cosre7@gmail.com>
2023-12-05 22:31:42 +0900

    feat : 같은 카드, 다른 카드 이벤트 추가

--- do/script.js
+++ do/script.js
@@ -4,9 +4,9 @@
 // unflip card
 
 const cards = document.querySelectorAll(".card");
-console.log(cards);
-
 const orderList = [0, 1, 2, 3];
+let firstCard, secondCard;
+let hasFlippedCard = false;
 
 function shuffleArr(arr) {
     arr.forEach((_, index) => {
@@ -22,8 +22,44 @@ function shuffle() {
     })
 }
 
-function flipCard(e) {
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1200);
+}
+
+function disabledCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+}
+
+function checkForMatch() {
+    const isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+    if (isMatch) {
+        // disable card
+        disabledCards();
+    } else {
+        // unflip card
+        unflipCards();
+    }
+}
+
+function flipCard() {
     this.classList.add('flip');
+    // 기존에 뒤집힌 카드가 없을 때 -> 첫 번째 카드인 경우
+    if (!hasFlippedCard) { 
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    // 두 번째 카드인 경우 
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch();
 }
 
 cards.forEach((card) => {
