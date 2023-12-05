3cbe971734d4387f81015e15128af9b608eb9e53
cosre7 <cosre7@gmail.com>
2023-12-05 22:01:53 +0900

    feat : 카드 섞기 기능 추가

--- do/script.js
+++ do/script.js
@@ -1,1 +1,25 @@
-console.log('ddd');
+// shuffle
+// check for match
+// disable card
+// unflip card
+
+const cards = document.querySelectorAll(".card");
+console.log(cards);
+
+const orderList = [0, 1, 2, 3];
+
+function shuffleArr(arr) {
+    arr.forEach((_, index) => {
+        // index만 사용하고싶을 때 -> 앞의 값은 _ 처리 가능 
+        const randomIdx = Math.floor(Math.random() * arr.length);
+        [arr[randomIdx], arr[index]] = [arr[index], arr[randomIdx]];
+    })
+}
+function shuffle() {
+    shuffleArr(orderList);
+    cards.forEach((card, index) => {
+        card.style.order = orderList[index];
+    })
+}
+
+shuffle();
